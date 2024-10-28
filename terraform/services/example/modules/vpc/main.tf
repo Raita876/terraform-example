1eabcc3ea88cd81a8b3667e8ce808ceadcab3ef1
Raita Watanabe <watanaberit20171129@gmail.com>
2024-10-29 01:00:53 +0900

    [fix] modulw vpc

--- terraform/services/example/modules/vpc/main.tf
+++ terraform/services/example/modules/vpc/main.tf
@@ -1,7 +1,47 @@
 resource "aws_vpc" "vpc" {
-  cidr_block = var.cidr_block
+  cidr_block = var.vpc_cidr_block
+  tags = {
+    Name = "${var.name}_vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public_subnet_cidr_block
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = var.name
+    Name = "${var.name}_public_subnet"
   }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.private_subnet_cidr_block
+
+  tags = {
+    Name = "${var.name}_private_subnet"
+  }
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.name}_internet_gateway"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.name}_public_route_table"
+  }
+}
+
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.internet_gateway.id
 }
--- terraform/services/example/stage/develop/main.tf
+++ terraform/services/example/stage/develop/main.tf
@@ -3,18 +3,9 @@ provider "aws" {
 }
 
 module "vpc" {
-  source     = "../../modules/vpc"
-  cidr_block = "10.0.0.0/16"
-  name       = "develop"
+  source                    = "../../modules/vpc"
+  vpc_cidr_block            = "10.0.0.0/16"
+  public_subnet_cidr_block  = "10.0.1.0/24"
+  private_subnet_cidr_block = "10.0.2.0/24"
+  name                      = "develop"
 }
-
-# resource "aws_subnet" "public_subnet" {
-#   vpc_id                  = aws_vpc.vpc.id
-#   cidr_block              = "10.0.1.0/24"
-#   map_public_ip_on_launch = true
-# }
-
-# resource "aws_subnet" "private_subnet" {
-#   vpc_id     = aws_vpc.vpc.id
-#   cidr_block = "10.0.2.0/24"
-# }
